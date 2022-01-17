0b15673ad616d73c97a52c41b2b358397f9271c3
alex <allexander0509@gmail.com>
2022-01-17 10:20:22 +0300

    Code reformat with linters

--- my_packages/deep_decorators/decorators_module.py
+++ my_packages/deep_decorators/decorators_module.py
@@ -2,14 +2,13 @@
 
 
 def multiple_execution(times=5):
-
     def executor(func):
-
         def wrapper(*args, **kwargs):
             for i in range(times):
                 yield func(*args, **kwargs)
 
         return wrapper
+
     return executor
 
 
@@ -18,5 +17,5 @@ def mul(a, b):
     return a * b
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print([i for i in mul(2, 3)])
