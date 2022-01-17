196149d330aaf78a068f62293c16ec3e9b93164c
alex <allexander0509@gmail.com>
2022-01-17 10:19:10 +0300

    Wrote a parametrized decorator

---
+++ my_packages/deep_decorators/decorators_module.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+def multiple_execution(times=5):
+
+    def executor(func):
+
+        def wrapper(*args, **kwargs):
+            for i in range(times):
+                yield func(*args, **kwargs)
+
+        return wrapper
+    return executor
+
+
+@multiple_execution(10)
+def mul(a, b):
+    return a * b
+
+
+if __name__ == '__main__':
+    print([i for i in mul(2, 3)])
